59db1941f8d43188ac08fa8e39be9f5e07947518
Wambuinganga <47768567+Wambuinganga@users.noreply.github.com>
2019-05-02 18:36:51 +0300

    Add files via upload

---
+++ Example5/shapes.py
@@ -0,0 +1,65 @@
+
+class Circle:
+	def __init__(self,radius):
+	    self.radius=radius
+	
+
+	def area(self,radius):
+		area=(22/7)*radius*radius
+		return area
+
+	def circumference(self,radius):
+		circumference=2*(22/7)*radius
+		return circumference
+
+
+class Square:
+	def __init__(self,side):
+
+	    self.side=side
+
+	def Area(self,length):
+		a=length
+		Area=a*a
+		return Area
+	def perimeter(self,length):
+		a=length
+		perimeter=a + a + a + a
+		return P
+
+class Rectangle:
+	def __init__(self,width,length):
+		self.width=width
+		self.length=length
+	    
+
+	def rec_area(self):
+		Area=width*length
+		return Area
+
+	def rec_perimeter(self):
+		perimeter=2(length+width)
+		return perimeter
+
+class Sphere:
+	def __init__(self,r):
+	    self.r=r
+
+	def surface_area(self):
+		surface_area=4*(22/7)*r*r
+		return surface_area
+
+	def volume(self):
+		volume=(4/3)*(22/7)*r*r*r
+		return volume
+
+		
+	    	   
+		
+
+
+
+		
+
+
+
